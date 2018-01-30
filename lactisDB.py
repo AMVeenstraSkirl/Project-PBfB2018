eb04a55f54db4220faab0821506ed7469fa794da
AMVeenstraSkirl <a.m.veenstra-skirl@rug.nl>
2018-01-30 14:02:02 +0100

    can write results to file

--- lactisDB.py
+++ lactisDB.py
@@ -9,8 +9,12 @@ import MySQLdb		#for the interactions with MySQL
 MyConnection = MySQLdb.connect( host = "localhost", user = "root", passwd = "PBfB2018", db = "lactis")
 MyCursor = MyConnection.cursor()
 
+
 #input = raw_input("Please enter your search string")		#Asks for an expression to search
 
+#preparing to write the results in a file
+ResultFileName = "SearchResults.txt"
+ResultFile = open(ResultFileName, 'w')
 
 #Execute an SQL command
 #SQL = "SELECT * FROM lactis WHERE owner LIKE '%s';" % (input)		#search command
@@ -25,20 +29,34 @@ print "Device    \tLocation\towner\tspecies\tstrain\tplasmid\tinfo\tabm\tgenotyp
 print "-----------------------------------------------------------------------------------------------"
 print
 
+#The header of the file
+ResultFile.write("""
 
-FirstResults=MyCursor.fetchall()                                #A tuple that contains the search results
+Your Search: SELECT * FROM lactis WHERE owner LIKE '%Skirl'
+
+Number of hits: %d 
+
+-----------------------------------------------------------------------------------------------
+Device    \tLocation\towner\tspecies\tstrain\tplasmid\tinfo\tabm\tgenotype\tdatum
+-----------------------------------------------------------------------------------------------
+
+""")
+
+
+FirstResults=MyCursor.fetchall()                                #variable that contains the search results
 
 #for loop like in the book:
-
 for Index in range(SQLresult):
 	#create a list with only relevant columns, resorted
 	entry=[FirstResults[Index][0],FirstResults[Index][15],FirstResults[Index][12],FirstResults[Index][8],FirstResults[Index][5],FirstResults[Index][4],FirstResults[Index][11],FirstResults[Index][9],FirstResults[Index][10],FirstResults[Index][18]]
-	#print the list
-	print "{e[0]}\t{e[1]}\t{e[2]}\t{e[3]:15}\t{e[4]:15}\t{e[5]}\t{e[6]}\t{e[7]}\t{e[8]}\t{e[9]}".format(e=entry)
+	#print the list for the current line
+	ResultLine = "{e[0]}\t{e[1]}\t{e[2]}\t{e[3]:15}\t{e[4]:15}\t{e[5]}\t{e[6]}\t{e[7]}\t{e[8]}\t{e[9]}".format(e=entry)
+	print ResultLine
+	#write the line to the destination file
+	ResultFile.write(ResultLine+"\n")
 
-#example line
-#'freezer_E', '13', 'G', '01', '', 'MG1363', 'none', 61588L, 'L. lactis', '', '', '', 'Anne-Marie Veenstra-Skirl', '', '', '13G01', 'workbox', '', '01-05-2017', 'N', 'N', '', '', '', '-', '', '', '', '', ''
-
+#close the written file
+ResultFile.close()
 
 #close the conncection to the SQL server
 MyCursor.close()
